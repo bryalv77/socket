b23bd2ed3db6908a40e67600e4d0450069d47e99
Bryan Alvarado <hola@bryanalvarado.com>
2022-03-05 00:16:37 +0100

    another commit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const cors = require('cors')
-require('dot-env')();
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
