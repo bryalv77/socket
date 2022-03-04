89a54f4da97ac91154b865837905b322feb1fd21
Bryan Alvarado <hola@bryanalvarado.com>
2022-03-05 00:11:34 +0100

    another commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const socketIO = require('socket.io');
+const cors = require('cors')
 require('dot-env')();
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express();
+server.use(cors());
 server.use((req, res) => {
   res.sendFile(INDEX, { root: __dirname });
 }).listen(PORT, () => console.log(`Listening on ${PORT}`));
