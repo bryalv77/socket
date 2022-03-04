e7ea49ff6309abc7160c61e086326e633157af97
Bryan Alvarado <hola@bryanalvarado.com>
2022-03-04 23:53:56 +0100

    adding logs

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const io = require('socket.io')();
-
+console.log('Starting...');
 const users = {}
 
 io.on('connection', socket => {
+  console.log('connected');
   socket.on('new-user', name => {
+    console.log('new user', name);
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
   })
   socket.on('send-chat-message', message => {
+    console.log('send message', message);
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
   })
   socket.on('disconnect', () => {
+    console.log('disconnected');
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
   })
